2497bb1fe644ce22850f325f06cd9d4ff8498ac8
alexlascarez0411 <alexlascarez10.al@gmail.com>
2026-09-08 11:13:28 -0600

    creacion modelo Vehiculo

---
+++ app/Models/Vehiculo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Vehiculo extends Model
+{
+    use HasFactory;
+
+    // Nombre de la tabla en la base de datos
+    protected $table = 'vehiculos';
+
+    // Campos permitidos para guardar
+    protected $fillable = [
+        'placa',
+        'marca',
+        'modelo',
+        'anno',
+        'kilometraje',
+        'categoria_id',
+        'estado_id',
+    ];
+
+    // Convertir tipos de datos a numeros enteros
+    protected $casts = [
+        'anno' => 'integer',
+        'kilometraje' => 'integer',
+    ];
+
+    // un carro pertenece a una categoría
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    // un vehículo pertenece a un estado
+    public function estado(): BelongsTo
+    {
+        return $this->belongsTo(Estado::class);
+    }
+
+    // un vehículo puede tener muchas rentas
+    public function rentas(): HasMany
+    {
+        return $this->hasMany(Renta::class);
+    }
+}

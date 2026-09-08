7511b56e949a007c58c94ff859802196301d7d07
chrischaves0710-droid <chrischaves0710@gmail.com>
2026-09-08 11:31:10 -0600

    Estado Seeder

---
+++ database/seeders/EstadoSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Estado;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EstadoSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $estados = ['Disponible', 'Alquilado', 'En Mantenimiento', 'Inactivo'];
+
+        foreach ($estados as $nombreEstado) {
+            Estado::firstOrCreate(['nombre' => $nombreEstado]);
+        }
+    }
+}

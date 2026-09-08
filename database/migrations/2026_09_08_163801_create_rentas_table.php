4be61273d57a6860d9816739bb499929b7015aa6
EddierRojas <EDDIER.ROJAS@ucr.ac.cr>
2026-09-08 10:54:53 -0600

    borrado de comentarios

--- database/migrations/2026_09_08_163801_create_rentas_table.php
+++ database/migrations/2026_09_08_163801_create_rentas_table.php
@@ -6,23 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::create('rentas', function (Blueprint $table) {
-            $table->id(); // Primary Key automática (id)
+            $table->id(); 
             
-            // Llaves foráneas
+            
             $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
             $table->foreignId('vehiculo_id')->constrained('vehiculos')->onDelete('cascade');
             
-            // Campos de fechas
+            
             $table->date('fecha_inicio');
             $table->date('fecha_fin');
             
-            // Campos numéricos/decimales
+            
             $table->decimal('precio_diario', 8, 2);
             $table->decimal('monto_total', 10, 2);
             
@@ -30,9 +28,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('rentas');

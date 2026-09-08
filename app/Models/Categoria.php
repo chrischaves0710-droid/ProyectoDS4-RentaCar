2c02d1c1d1d80a44d67182c192b0044ed93af0a7
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-08 11:11:49 -0600

    agregación de los datos del model Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+    /**
+     * La tabla asociada al modelo.
+    */
+  
+    protected $table = 'categorias';
+    /**
+     * Los atributos que son asignables de forma masiva.
+    */
+   
+    protected $fillable = [
+        'nombre',
+    ];
+
+    /**
+     * Obtiene todos los vehículos pertenecientes a esta categoría.
+     */
+    public function vehiculos(): HasMany
+    {
+        return $table = $this->hasMany(Vehiculo::class, 'categoria_id');
+    }
 }

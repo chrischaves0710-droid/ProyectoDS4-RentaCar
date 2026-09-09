e7cbfe1e22de05d439f25baf5c85bba65c0f86c8
ronaldmora2731-cloud <ronaldmora2731@gmail.com>
2026-09-09 10:25:03 -0600

    web de clientes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AccesorioController;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\RentaController;
 use App\Http\Controllers\Api\VehiculosController;
+use App\Http\Controllers\Api\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,7 +12,7 @@ Route::get('/', function () {
 });
 
 // Rutas para el CRUD de vehiculos
-Route::resource('vehiculos', VehiculosController::class);
+    Route::resource('vehiculos', VehiculosController::class);
 
 // Rutas para el CRUD de Accesorios
 Route::resource('accesorios', AccesorioController::class);
@@ -21,3 +22,6 @@ Route::resource('rentas', RentaController::class);
 
 // Rutas para el CRUD de Categorías
 Route::resource('categorias', CategoriaController::class);
+
+// Rutas para el CRUD de Clientes
+Route::apiResource('clientes', ClienteController::class);

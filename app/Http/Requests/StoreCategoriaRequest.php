5b0f2a6846a6d44dc1d5495f5f284c876c05d208
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-11 20:23:54 -0600

    Separación del controlador de categoria, al CategotiaStoreRequest

--- app/Http/Requests/StoreCategoriaRequest.php
+++ app/Http/Requests/StoreCategoriaRequest.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCategoriaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'nombre' => 'required|string|max:255|unique:categorias,nombre',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'nombre.required' => 'El campo nombre es obligatorio.',
+            'nombre.string'   => 'El campo nombre debe ser una cadena de texto.',
+            'nombre.max'      => 'El campo nombre no debe superar los 255 caracteres.',
+            'nombre.unique'   => 'El nombre de la categoría ya se encuentra registrado.',
         ];
     }
 }

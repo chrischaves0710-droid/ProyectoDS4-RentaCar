2e0a67801c5c6e8829d152830dfe64b3738299e3
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-11 21:50:09 -0600

    actualizacion de excepcion

--- app/Exceptions/CategoriaException.php
+++ app/Exceptions/CategoriaException.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 
-class CategoriaException extends Exception
+class BusinessRuleException extends Exception
 {
     public function render($request)
     {

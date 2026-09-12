a663d2c69aaefff17042dbc24a8985da40c3992a
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-11 22:09:52 -0600

    test

--- tests/Unit/CategoriaServiceTest.php
+++ tests/Unit/CategoriaServiceTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use App\Exceptions\BusinessRuleException;
 use App\Models\Categoria;
+use App\Models\Cliente;
+use App\Models\Renta;
 use App\Models\Vehiculo;
 use App\Services\CategoriaService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -21,24 +23,29 @@ class CategoriaServiceTest extends TestCase
         $this->service = new CategoriaService();
     }
 
-    /** @test */
-    public function regla_1_no_permite_crear_categorias_reservadas()
+    public function test_regla_1_no_permite_crear_categoria_reservada()
     {
         $this->expectException(BusinessRuleException::class);
         $this->service->crear(['nombre' => 'En Mantenimiento']);
     }
 
-    /** @test */
-    public function regla_2_no_permite_modificar_categoria_con_vehiculos_en_renta_activa()
+    public function test_regla_2_no_permite_modificar_categoria_con_vehiculo_en_renta_activa()
     {
         $categoria = Categoria::factory()->create();
-        
+        $vehiculo = Vehiculo::factory()->create(['categoria_id' => $categoria->id]);
+        $cliente = Cliente::factory()->create();
+
+        Renta::factory()->create([
+            'vehiculo_id' => $vehiculo->id,
+            'cliente_id'  => $cliente->id,
+            'fecha_fin'   => now()->addDays(5),
+        ]);
+
         $this->expectException(BusinessRuleException::class);
         $this->service->actualizar($categoria, ['nombre' => 'Nuevo Nombre']);
     }
 
-    /** @test */
-    public function regla_3_no_permite_eliminar_categoria_con_vehiculos_asociados()
+    public function test_regla_3_no_permite_eliminar_categoria_con_vehiculos()
     {
         $categoria = Categoria::factory()->create();
         Vehiculo::factory()->create(['categoria_id' => $categoria->id]);
@@ -47,8 +54,7 @@ class CategoriaServiceTest extends TestCase
         $this->service->eliminar($categoria);
     }
 
-    /** @test */
-    public function regla_4_no_permite_eliminar_categoria_principal_id_1()
+    public function test_regla_4_no_permite_eliminar_categoria_base_id_1()
     {
         $categoria = Categoria::factory()->create(['id' => 1]);
 

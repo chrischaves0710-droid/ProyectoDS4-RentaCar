7fb5b9e7804da789224794baf504588bf5fa4b0f
chrischaves0710-droid <chrischaves0710@gmail.com>
2026-09-13 14:11:00 -0600

    LLamada del Estado en Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\EstadoController;
 use App\Http\Controllers\Api\VehiculoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('vehiculos', VehiculoController::class);
+Route::apiResource('estados', EstadoController::class);

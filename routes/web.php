7fb08c5ed66d5bd937f640c1efc8d79e29fb99dc
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-13 16:20:37 -0600

    api

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::resource('vehiculos', VehiculoController::class);
 Route::resource('accesorios', AccesorioController::class);
 Route::resource('rentas', RentaController::class);
 Route::resource('estados', EstadoController::class);
-Route::apiResource('categorias', CategoriaController::class);
+Route::Resource('categorias', CategoriaController::class);
 Route::apiResource('clientes', ClienteController::class);
 
 // Rutas de reportes y consultas

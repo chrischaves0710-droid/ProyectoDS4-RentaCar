cccfaefb3d7ba7b7859ec802e953efbb8ca94f11
sgabrieleiton <sgabrieleiton@gmail.com>
2026-09-13 16:29:23 -0600

    apiResource categoria

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\EstadoController;
 use App\Http\Controllers\Api\VehiculoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('vehiculos', VehiculoController::class);
 Route::apiResource('estados', EstadoController::class);
+Route::apiResource('categorias', CategoriaController::class);

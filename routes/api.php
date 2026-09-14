457f0c456f32eb6d78bd34f0359b9fb33a49bf2f
EddierRojas <EDDIER.ROJAS@ucr.ac.cr>
2026-09-14 12:39:51 -0600

    api y cambios en composer

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Api\EstadoController;
 use App\Http\Controllers\Api\ClienteController;
 use App\Http\Controllers\Api\VehiculoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RentaController;
 
+Route::apiResource('rentas', RentaController::class);
 Route::apiResource('vehiculos', VehiculoController::class);
 Route::apiResource('estados', EstadoController::class);
 Route::apiResource('categorias', CategoriaController::class);

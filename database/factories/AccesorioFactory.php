13dd55fdc3c3573824bc4e1290b23831e381ab5b
chrischaves0710-droid <chrischaves0710@gmail.com>
2026-09-14 22:05:25 -0600

    Configuracion de AccesorioFactory

--- database/factories/AccesorioFactory.php
+++ database/factories/AccesorioFactory.php
@@ -2,33 +2,15 @@
 
 namespace Database\Factories;
 
-use App\Models\Accesorio;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Accesorio>
- */
 class AccesorioFactory extends Factory
 {
-    /**
-     * 
-     *
-     * @var string
-     */
-    protected $model = Accesorio::class;
-
-    /**
-     * Define el estado por defecto del modelo.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            // Genera un nombre de 2 palabras único con inicial mayúscula
-            'nombre' => ucfirst(fake()->unique()->words(2, true)),
-            // Genera un precio aleatorio entre 5.00 y 50.00 con 2 decimales
-            'precio_unitario' => fake()->randomFloat(2, 5, 50),
+            'nombre' => fake()->unique()->words(2, true),
+            'precio_unitario' => fake()->randomFloat(2, 500, 10000),
         ];
     }
 }

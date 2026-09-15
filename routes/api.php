3e5ae7e9267ecf248e5d41e144aef64904865c02
chrischaves0710-droid <chrischaves0710@gmail.com>
2026-09-14 22:04:07 -0600

    Configuracion de Accesorio en API

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AccesorioController;
 use App\Http\Controllers\Api\CategoriaController;
-use App\Http\Controllers\Api\EstadoController;
 use App\Http\Controllers\Api\ClienteController;
+use App\Http\Controllers\Api\EstadoController;
+use App\Http\Controllers\Api\RentaController;
 use App\Http\Controllers\Api\VehiculoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RentaController;
 
 Route::apiResource('rentas', RentaController::class);
 Route::apiResource('vehiculos', VehiculoController::class);
 Route::apiResource('estados', EstadoController::class);
 Route::apiResource('categorias', CategoriaController::class);
 Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('accesorios', AccesorioController::class);
+
+Route::post(
+    'rentas/{renta}/accesorios/{accesorio}',
+    [AccesorioController::class, 'agregarARenta']
+);

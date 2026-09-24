ce7520a247c67949fa830fdcd0ef8da8e7081255
chrischaves0710-droid <chrischaves0710@gmail.com>
2026-09-23 23:08:50 -0600

    Creacion del RoleSeeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $roles = [
+            'Admin_General',
+            'Admin_Inventarios',
+            'Gestor_Rentas',
+            'Cliente',
+        ];
+
+        foreach ($roles as $rol) {
+            Role::firstOrCreate([
+                'name' => $rol,
+                'guard_name' => 'web',
+            ]);
+        }
+    }
+}
